f8cd14602a199b5a7e1b9a55799ecb75fdc8684c
jvialu03 <j_vialu@fanshaweonline.ca>
2020-03-27 18:45:15 -0400

    returns

--- js/main.js
+++ js/main.js
@@ -7,8 +7,13 @@
   function changeColor(elem, color) {
     // theBox.style.backgroundColor = this.dataset.color;
     console.log(elem, color);
-
     document.querySelector(`${elem}`).style.backgroundColor = color;
+
+    reportChange(elem, color);
+  }
+
+  function reportChange(el, col) {
+    console.log(`You changed the background color of ${el} to ${col}`);
   }
 
   // event handling at the bottom
